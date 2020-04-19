8b983f0b665c506b3eaad8506cbc6e7aa32197c9
Boris Belmar <borisbelmarm@gmail.com>
2020-04-19 17:46:56 -0400

    (WIP) Redesign activity register expense

--- app/src/main/java/com/example/simplemonkey/ExpenseCreate.java
+++ app/src/main/java/com/example/simplemonkey/ExpenseCreate.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NuevoGasto extends AppCompatActivity {
+public class ExpenseCreate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nuevo_gasto);
+        setContentView(R.layout.activity_expense_create);
     }
 }
