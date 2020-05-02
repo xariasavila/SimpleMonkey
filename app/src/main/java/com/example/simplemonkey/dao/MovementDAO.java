b38bfd1d7942d94e4fee65457e5ea71695fd7332
Boris Belmar <borisbelmarm@gmail.com>
2020-05-02 19:31:31 -0400

    Refactor Expenses and incomes to Movement

--- app/src/main/java/com/example/simplemonkey/dao/MovementDAO.java
+++ app/src/main/java/com/example/simplemonkey/dao/MovementDAO.java
@@ -4,49 +4,51 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.example.simplemonkey.BuildConfig;
-import com.example.simplemonkey.R;
 import com.example.simplemonkey.controller.AdminSQLiteOpenHelper;
-import com.example.simplemonkey.model.Expense;
+import com.example.simplemonkey.model.Movement;
 import com.example.simplemonkey.utils.DateConvert;
 import com.example.simplemonkey.utils.PrimaryKeyGenerator;
 
 import java.util.ArrayList;
 import java.util.Date;
 
-public class ExpenseDAO implements InterfaceDAO<Expense> {
+public class MovementDAO implements InterfaceDAO<Movement> {
 
     private Context context;
+    private int uid;
     private AdminSQLiteOpenHelper admin;
     private String dbName = BuildConfig.DB_NAME;
-    private String tableName = "expense";
-    private int uid = 000;
+    private String tableName = "movement";
 
-    public ExpenseDAO(Context context) {
+    public MovementDAO(Context context, int uid) {
         this.context = context;
+        this.uid = uid;
     }
 
     @Override
-    public boolean insert(Expense expense) {
+    public boolean insert(Movement movement) {
         admin = new AdminSQLiteOpenHelper(context, dbName, null, 1);
         SQLiteDatabase database = admin.getWritableDatabase();
         ContentValues registry = new ContentValues();
 
-        registry.put("id", PrimaryKeyGenerator.generate());
-        registry.put("uid", expense.getUid());
-        registry.put("name", expense.getName());
-        registry.put("description", expense.getDescription());
-        registry.put("date", expense.getDateToString());
-        registry.put("amount", expense.getAmount());
-        registry.put("currency", expense.getCurrency());
-        registry.put("coordinates", expense.getCoordinates());
-        registry.put("payday", expense.getPayday());
-        registry.put("fee_number", expense.getFeeNumber());
-        registry.put("category_id", expense.getCategoryId());
-        registry.put("borrow_id", expense.getBorrowId());
-        registry.put("debt_id", expense.getDebtId());
-        registry.put("budget_id", expense.getBudgetId());
+        registry.put("id", PrimaryKeyGenerator.generate(movement.getUid()));
+        registry.put("uid", movement.getUid());
+        registry.put("name", movement.getName());
+        registry.put("description", movement.getDescription());
+        registry.put("date", movement.getDateToString());
+        registry.put("amount", movement.getAmount());
+        registry.put("currency", movement.getCurrency());
+        registry.put("coordinates", movement.getCoordinates());
+        registry.put("payday", movement.getPayday());
+        registry.put("fee_number", movement.getFeeNumber());
+        registry.put("id_category", movement.getCategoryId());
+        registry.put("id_borrow", movement.getBorrowId());
+        registry.put("id_debt", movement.getDebtId());
+        registry.put("id_budget", movement.getBudgetId());
+        registry.put("income", movement.isIncome());
 
         if(database.insert(tableName, null, registry) != -1) {
             database.close();
@@ -58,13 +60,15 @@ public class ExpenseDAO implements InterfaceDAO<Expense> {
     }
 
     @Override
-    public ArrayList<Expense> findAll() {
+    public ArrayList<Movement> findAll() {
         admin = new AdminSQLiteOpenHelper(context, dbName, null, 1);
         SQLiteDatabase database = admin.getWritableDatabase();
 
-        Cursor rows = database.rawQuery("SELECT id, uid, name, description, date, amount, currency, fee_number, payday, done, sync, coordinates, created_at, updated_at, id_category, id_budget, id_debt, id_borrow FROM " + tableName + " WHERE uid = " + uid + " ORDER BY date ASC", null);
+        Log.d("DAO.findAll", "QUERY TEST");
+        Cursor rows = database.rawQuery("SELECT id, uid, name, description, date, amount, currency, fee_number, payday, done, sync, coordinates, created_at, updated_at, id_category, id_budget, id_debt, id_borrow, income FROM " + tableName + " WHERE uid = " + uid + " ORDER BY date ASC", null);
+        Log.d("DAO.findAll", "QUERY PASSED");
 
-        ArrayList<Expense> resultList = new ArrayList<>();
+        ArrayList<Movement> resultList = new ArrayList<>();
 
         while(rows.moveToNext()) {
             int id = Integer.parseInt(rows.getString(0));
@@ -85,8 +89,9 @@ public class ExpenseDAO implements InterfaceDAO<Expense> {
             int budgetId = Integer.parseInt(rows.getString(15));
             int debtId = Integer.parseInt(rows.getString(16));
             int borrowId = Integer.parseInt(rows.getString(17));
+            boolean income = Boolean.getBoolean(rows.getString(18));
 
-            Expense evaluation = new Expense(
+            Movement evaluation = new Movement(
                     id,
                     uid,
                     name,
@@ -98,6 +103,7 @@ public class ExpenseDAO implements InterfaceDAO<Expense> {
                     coordinates,
                     createdAt,
                     updatedAt,
+                    income,
                     feeNumber,
                     payday,
                     done,
@@ -114,11 +120,11 @@ public class ExpenseDAO implements InterfaceDAO<Expense> {
     }
 
     @Override
-    public Expense findById(int id) {
+    public Movement findById(int id) {
         admin = new AdminSQLiteOpenHelper(context, dbName, null, 1);
         SQLiteDatabase database = admin.getWritableDatabase();
 
-        Cursor row = database.rawQuery("SELECT name, description, date, amount, currency, fee_number, payday, done, sync, coordinates, created_at, updated_at, id_category, id_budget, id_debt, id_borrow FROM " + tableName + " WHERE uid = " + uid + " AND id = " + id + " ORDER BY date ASC", null);
+        Cursor row = database.rawQuery("SELECT name, description, date, amount, currency, fee_number, payday, done, sync, coordinates, created_at, updated_at, id_category, id_budget, id_debt, id_borrow, income FROM " + tableName + " WHERE uid = " + uid + " AND id = " + id + " ORDER BY date ASC", null);
         if(row.moveToFirst()) {
             String name = row.getString(0);
             String description = row.getString(1);
@@ -136,10 +142,11 @@ public class ExpenseDAO implements InterfaceDAO<Expense> {
             int budgetId = Integer.parseInt(row.getString(13));
             int debtId = Integer.parseInt(row.getString(14));
             int borrowId = Integer.parseInt(row.getString(15));
+            boolean income = Boolean.getBoolean(row.getString(16));
 
             database.close();
 
-            return new Expense(
+            return new Movement(
                     id,
                     uid,
                     name,
@@ -151,6 +158,7 @@ public class ExpenseDAO implements InterfaceDAO<Expense> {
                     coordinates,
                     createdAt,
                     updatedAt,
+                    income,
                     feeNumber,
                     payday,
                     done,
@@ -166,27 +174,28 @@ public class ExpenseDAO implements InterfaceDAO<Expense> {
     }
 
     @Override
-    public boolean update(Expense expense) {
+    public boolean update(Movement movement) {
 
         admin = new AdminSQLiteOpenHelper(context, dbName, null, 1);
         SQLiteDatabase database = admin.getWritableDatabase();
         ContentValues registry = new ContentValues();
 
-        registry.put("uid", expense.getUid());
-        registry.put("name", expense.getName());
-        registry.put("description", expense.getDescription());
-        registry.put("date", expense.getDateToString());
-        registry.put("amount", expense.getAmount());
-        registry.put("currency", expense.getCurrency());
-        registry.put("coordinates", expense.getCoordinates());
-        registry.put("payday", expense.getPayday());
-        registry.put("fee_number", expense.getFeeNumber());
-        registry.put("category_id", expense.getCategoryId());
-        registry.put("borrow_id", expense.getBorrowId());
-        registry.put("debt_id", expense.getDebtId());
-        registry.put("budget_id", expense.getBudgetId());
+        registry.put("uid", movement.getUid());
+        registry.put("name", movement.getName());
+        registry.put("description", movement.getDescription());
+        registry.put("date", movement.getDateToString());
+        registry.put("amount", movement.getAmount());
+        registry.put("currency", movement.getCurrency());
+        registry.put("coordinates", movement.getCoordinates());
+        registry.put("payday", movement.getPayday());
+        registry.put("fee_number", movement.getFeeNumber());
+        registry.put("id_category", movement.getCategoryId());
+        registry.put("id_borrow", movement.getBorrowId());
+        registry.put("id_debt", movement.getDebtId());
+        registry.put("id_budget", movement.getBudgetId());
+        registry.put("income", movement.isIncome());
 
-        boolean isUpdated = database.update(tableName, registry, "id = ?", new String[] {Integer.toString(expense.getId())}) > 0;
+        boolean isUpdated = database.update(tableName, registry, "id = ?", new String[] {Integer.toString(movement.getId())}) > 0;
 
         if(isUpdated) {
             database.close();
