c0302ef8d772c53768ef33425b7a19ca6da7a227
Boris Belmar <borisbelmarm@gmail.com>
2020-05-02 19:32:53 -0400

    Add MainActivity Prototype

--- app/src/main/java/com/example/simplemonkey/MainActivity.java
+++ app/src/main/java/com/example/simplemonkey/MainActivity.java
@@ -2,13 +2,144 @@ package com.example.simplemonkey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.simplemonkey.adapter.MovementAdapter;
+import com.example.simplemonkey.dao.MovementDAO;
+import com.example.simplemonkey.model.Movement;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvMovements;
+
+    private ArrayList<Movement> movements;
+    private MovementAdapter movementAdapter;
+    private MovementDAO dao;
+    private SharedPreferences preferences;
+
+    private static String TAG = "MainActivity";
+
+    private float[] yData = {3000, 5500, 10500, 15000, 1000, 50, 540};
+    private String[] xData = {"Casa", "Jessica" , "Mohammad" , "Kelsey", "Sam", "Robert", "Ashley"};
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvMovements = findViewById(R.id.lvMovements);
+
+        preferences = getSharedPreferences("userData", Context.MODE_PRIVATE);
+        dao = new MovementDAO(MainActivity.this, preferences.getInt("uid", 000));
+
+        movements = dao.findAll();
+        Log.d(TAG, "FIND ALL PASSED");
+
+        movementAdapter = new MovementAdapter(MainActivity.this, movements);
+        lvMovements.setAdapter(movementAdapter);
+
+        Log.d(TAG, "onCreate: starting to create chart");
+
+        pieChart = (PieChart) findViewById(R.id.piechart);
+
+        pieChart.setRotationEnabled(true);
+        //pieChart.setUsePercentValues(true);
+        //pieChart.setHoleColor(Color.BLUE);
+        //pieChart.setCenterTextColor(Color.BLACK);
+        pieChart.setHoleRadius(25f);
+        pieChart.setTransparentCircleAlpha(0);
+        pieChart.setCenterText("Gastos");
+        pieChart.setCenterTextSize(12);
+        //pieChart.setDrawEntryLabels(true);
+        //pieChart.setEntryLabelTextSize(20);
+        //More options just check out the documentation!
+
+        addDataSet();
+
+        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                Log.d(TAG, "onValueSelected: Value select from chart.");
+                Log.d(TAG, "onValueSelected: " + e.toString());
+                Log.d(TAG, "onValueSelected: " + h.toString());
+                Toast.makeText(MainActivity.this, h.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MovementCreate.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void addDataSet() {
+        Log.d(TAG, "addDataSet started");
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
+
+        for(int i = 0; i < yData.length; i++){
+            yEntrys.add(new PieEntry(yData[i] , i));
+        }
+
+        for(int i = 1; i < xData.length; i++){
+            xEntrys.add(xData[i]);
+        }
+
+        //create the data set
+        PieDataSet pieDataSet = new PieDataSet(yEntrys, "Categorías");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        //add colors to dataset
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.rgb(235, 189, 52));
+        colors.add(Color.rgb(235, 150, 30));
+        colors.add(Color.rgb(235, 130, 20));
+        colors.add(Color.rgb(220, 200, 80));
+        colors.add(Color.rgb(250, 120, 50));
+        colors.add(Color.rgb(220, 150, 30));
+        colors.add(Color.rgb(220, 128, 40));
+
+        pieDataSet.setColors(colors);
+
+        //add legend to chart
+        Legend legend = pieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+
+        //create pie data object
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
     }
 }
